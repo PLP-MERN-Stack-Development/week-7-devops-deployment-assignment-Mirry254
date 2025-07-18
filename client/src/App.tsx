7d130af65908ec3fbf81d42fc50f9947bb8d218f
Mirry254 <mwongelimirriam01@gmail,com>
2025-07-18 16:06:18 +0300

    week 7 devops deployment

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Week 7 DevOps Assignment</h1>
+      <p>This is the React frontend.</p>
+    </div>
+  );
+}
+
+export default App;
+
